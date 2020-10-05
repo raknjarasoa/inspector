d31b465d1b0c09e2d08c21ce7564836d3fa498bd
Dharmen Shah <dharmen.s@milestoneinternet.com>
2020-10-05 13:46:31 +0530

    test: 💍 fix test

--- projects/ngneat/inspector/src/lib/inspector.component.spec.ts
+++ projects/ngneat/inspector/src/lib/inspector.component.spec.ts
@@ -1,9 +1,7 @@
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserTestingModule } from '@angular/platform-browser/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Spectator, createComponentFactory } from '@ngneat/spectator';
-import { TabsModule } from 'ngx-bootstrap/tabs';
 
 import { InspectorComponent } from './inspector.component';
 
@@ -11,7 +9,7 @@ describe('InspectorComponent', () => {
   let spectator: Spectator<InspectorComponent>;
   const createComponent = createComponentFactory({
     component: InspectorComponent,
-    imports: [BrowserTestingModule, HttpClientTestingModule, DragDropModule, FontAwesomeModule, TabsModule.forRoot()],
+    imports: [BrowserTestingModule, DragDropModule, FontAwesomeModule],
   });
 
   it('should create', () => {
