0eff4458ace9f0bdb0a0ab25cde8864d8f374eda
Dharmen Shah <dharmen.s@milestoneinternet.com>
2020-10-23 11:56:26 +0530

    fix: 🐛 remove monaco editor from demo app

--- projects/ngneat-inspector-demo/src/app/app.module.ts
+++ projects/ngneat-inspector-demo/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InspectorModule } from '@ngneat/inspector';
-import { CodeEditorModule } from '@ngstack/code-editor';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
@@ -10,12 +9,7 @@ import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, ChildComponent],
-  imports: [
-    BrowserModule,
-    environment.production ? [] : InspectorModule.forRoot(),
-    BrowserAnimationsModule,
-    CodeEditorModule.forRoot(),
-  ],
+  imports: [BrowserModule, environment.production ? [] : InspectorModule.forRoot(), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
